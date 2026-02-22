232e629103f1aa7795fb17cfe6ff9f6f73026a83
Hasnat alam <hasnatalamofficial@gmail.com>
2026-02-22 10:58:31 +0530

    some addition

---
+++ Practice01/src/com/app/basic/CommandTest.java
@@ -0,0 +1,9 @@
+public class CommandTest{
+	public static void main(String [] args){
+		
+		System.out.println(args[0]);
+		int a = Integer.parseInt(args[1]);
+		int b = Integer.parseInt(args[2]);
+		System.out.println(a+b);
+	}
+}
